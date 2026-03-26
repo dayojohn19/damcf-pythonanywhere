16f2be29e660d6f5309c289b375f45c102a49cc3
digital life <digitallife@digitals-MacBook-Pro.local>
2026-03-27 06:31:38 +0800

    allowed host

--- config/settings_sqlite_export.py
+++
@@ -1,9 +0,0 @@
-from .settings import *
-
-# Override DB for one-off export/import into SQLite.
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "database.sqlite3",
-    }
-}
